0cfd1cf8fac9956e0364458638b2469e6c852b4e
Travis Riggs <travis.riggs@lexmark.com>
2017-02-06 09:54:24 -0500

    Added setup and requirements to project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from disutils.core import setup
+
+setup(
+    name='Algorithms',
+    version='0.1dev',
+    packages=['mergesort',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
