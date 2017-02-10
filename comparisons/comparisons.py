c41315a0131542911dbd80651453999f09c4cbdf
Travis Riggs <travis.riggs@lexmark.com>
2017-02-10 15:52:31 -0500

    Implemented the count_inversions function
    Calls a different function to perform the recursive sorting
    and inversion counting: sort_and_count_inversions()

--- comparisons/comparisons.py
+++ comparisons/comparisons.py
@@ -19,14 +19,70 @@ def count_inversions(aList):
 
     inversionCount = None
 
-    # Check base case
-
-    # Call recursively for first half of list
-
-    # Call recursively for second half of list
-
-    # Count split inversions
-
-    # Total up all inversions
+    # Get the inversion count
+    inversionCount, sortedList = sort_and_count_inversions(aList)
 
     return inversionCount
+
+
+def sort_and_count_inversions(aList):
+    """Return an inversion count and sorted list"""
+
+    inversionCount = 0
+    sortedList = []
+
+    n = len(aList)
+
+    # Check base case
+    if n <= 1:
+        # If the list has 1 or 0 elements, there are no inversions
+        #  and nothing to sort
+        return 0, aList
+
+    # Recursively call for first half of list
+    firstCount, firstList = sort_and_count_inversions(aList[0:n/2])
+
+    # Recursively call for second half of list
+    secondCount, secondList = sort_and_count_inversions(aList[n/2:])
+
+    # Merge the two lists together while looking for split inversions
+    firstLength = len(firstList)
+    secondLength = len(secondList)
+    i = 0
+    j = 0
+
+    for z in range(n):
+
+        # Make sure we won't try to access past the end of the array
+        #  If we've reachd the end of the first array, then
+        #  add the element from the second array.
+        if i == firstLength:
+            sortedList.append(secondList[j])
+            j += 1
+
+        # If we've reached the end of the second array, then add
+        #  the element from the first array
+        elif j == secondLength:
+            sortedList.append(firstList[i])
+            i += 1
+
+        # The normal case (before we've reached the end of the arrays)
+        elif firstList[i] < secondList[j]:
+            sortedList.append(firstList[i])
+            i += 1
+
+        else:
+            sortedList.append(secondList[j])
+            j += 1
+            # Here are some split inversions!
+            #  ...which is equal to the number of items remaining
+            #  in the first list.
+            inversionCount += firstLength - i
+
+
+    # Add the non-split inversions for the final total of inversions
+    inversionCount += firstCount + secondCount
+
+
+    return inversionCount, sortedList
+
