51eafb130556f54cc832b2f46631d9ab2d5d11fc
Travis Riggs <travis.riggs@lexmark.com>
2017-02-10 15:59:19 -0500

    Added script to count inversions from a file

---
+++ countinversions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""
+
+        Count the number of split inversions
+        from numbers in a text file.
+         by Travis Riggs
+
+"""
+import pdb
+import comparisons
+
+
+if __name__ == "__main__":
+
+    # Load all of the numbers from the text file into a list
+    with open("/Users/travis/Desktop/IntegerArray.txt", "r") as f:
+
+        numbers = []
+
+        for line in f.readlines():
+            numbers.append(int(line))
+
+
+    # Count the number of inversions in the list
+    inversions = comparisons.count_inversions(numbers)
+
+    print("Inversion count = " + str(inversions))
