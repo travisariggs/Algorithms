5620360647310346915f6269f63342c59f142bc0
Travis Riggs <travis.riggs@lexmark.com>
2017-02-15 13:39:35 -0500

    Created stub code for quick_sort function

---
+++ sorting/quicksort.py
@@ -0,0 +1,23 @@
+"""
+
+      Quicksort Algorithm
+       by Travis Riggs
+
+    This module implements the quicksort algorithm.
+
+"""
+
+def quick_sort(aList, startIndex=None, endIndex=None):
+    """Sort a list from least to greatest using quicksort"""
+
+    # Select the first element as the pivot
+
+    # Partition the list between elements greater than and less than
+    #  the pivot element
+
+
+    # Rescursively call quick_sort on the upper and lower partitions
+
+
+    # Return the sorted list
+    return aList
