0ce00c7685c8266f37333593167edbd793ec4f20
Travis Riggs <travis.riggs@lexmark.com>
2017-03-10 14:27:50 -0500

    Adding data file for strong connection analysis

---
+++ graphs/runstrongconnections.py
@@ -0,0 +1,16 @@
+"""
+
+        Run the Strongly Connected Components algorithm
+         by Travis Riggs
+
+    This runs a strongly connected component analysis algorithm
+    on a given graph edge file.
+
+"""
+
+from graphs import DirectedGraph
+
+if __name__ == '__main__':
+
+    pass
+    
